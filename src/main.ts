16afad7cc828f6c528e31ea328cff98a07a63f5d
Mykhailo Stepanov <bibipkins@gmail.com>
2022-06-15 21:32:01 +0300

    test 3

--- src/main.ts
+++ src/main.ts
@@ -40,7 +40,8 @@ const run = () => {
     const time = new Date().toTimeString();
     core.setOutput('time', time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
+
+    //const payload = JSON.stringify(github.context.payload, undefined, 2);
     //console.log(`The event payload: ${payload}`);
 
     const trxPath = core.getInput('test-results');
