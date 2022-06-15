7ff668e87c84d097ba0324057f7d603803335f69
Mykhailo Stepanov <bibipkins@gmail.com>
2022-06-15 21:36:43 +0300

    test

--- dist/index.js
+++ dist/index.js
@@ -8875,28 +8875,26 @@ const getAbsolutePaths = (fileNames, directoryName) => {
     return absolutePaths;
 };
 const getFiles = (trxPath) => {
+    console.log(`0`);
     if (!fs.existsSync(trxPath)) {
+        console.log(`No files =()`);
         return [];
     }
+    console.log(`1. Files exist`);
     const fileNames = fs.readdir(trxPath);
+    console.log(`2. Files count: ${fileNames.length}`);
     const trxFiles = fileNames.filter(f => f.endsWith('.trx'));
-    core.info(`Files count: ${fileNames.length}`);
+    console.log(`3. TRX Files count: ${fileNames.length}`);
     const filesWithAbsolutePaths = getAbsolutePaths(trxFiles, trxPath);
-    filesWithAbsolutePaths.forEach(f => core.info(`File: ${f}`));
+    console.log(`4`);
+    filesWithAbsolutePaths.forEach(f => console.log(`File: ${f}`));
+    console.log(`5`);
     return filesWithAbsolutePaths;
 };
 const run = () => {
     try {
-        // `who-to-greet` input defined in action metadata file
-        const nameToGreet = core.getInput('who-to-greet');
-        console.log(`Hello ${nameToGreet}!`);
-        const time = new Date().toTimeString();
-        core.setOutput('time', time);
-        // Get the JSON webhook payload for the event that triggered the workflow
-        const payload = JSON.stringify(github.context.payload, undefined, 2);
-        console.log(`The event payload: ${payload}`);
         const trxPath = core.getInput('test-results');
-        console.log(`Path: ${trxPath}`);
+        console.log(`TRX Path: ${trxPath}`);
         getFiles(trxPath);
     }
     catch (error) {
