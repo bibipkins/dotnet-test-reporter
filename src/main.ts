b54e03ac72260b2a45ab5786d46261aad018de68
Mykhailo Stepanov <bibipkins@gmail.com>
2022-06-15 21:40:21 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -16,28 +16,23 @@ const getAbsolutePaths = (fileNames: string[], directoryName: string): string[] 
 };
 
 const getFiles = (trxPath: string): string[] => {
-  console.log(`0`);
+  console.log(`TRX Path: ${trxPath}`);
   if (!fs.existsSync(trxPath)) {
-    console.log(`No files =()`);
     return [];
   }
 
-  console.log(`1. Files exist`);
   const fileNames = fs.readdirSync(trxPath);
-  console.log(`2. Files count: ${fileNames.length}`);
+  console.log(`Files count: ${fileNames.length}`);
   const trxFiles = fileNames.filter(f => f.endsWith('.trx'));
-  console.log(`3. TRX Files count: ${fileNames.length}`);
+  console.log(`TRX Files count: ${trxFiles.length}`);
   const filesWithAbsolutePaths = getAbsolutePaths(trxFiles, trxPath);
-  console.log(`4`);
   filesWithAbsolutePaths.forEach(f => console.log(`File: ${f}`));
-  console.log(`5`);
   return filesWithAbsolutePaths;
 };
 
 const run = () => {
   try {
     const trxPath = core.getInput('test-results');
-    console.log(`TRX Path: ${trxPath}`);
     getFiles(trxPath);
   } catch (error: any) {
     core.setFailed(error.message);
