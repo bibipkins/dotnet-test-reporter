3fe99d5cb0e007ad61ea59e0da7c2e1a1cfdbf10
Mykhailo Stepanov <bibipkins@gmail.com>
2022-06-16 11:01:34 +0300

    Test reading a file (#3)
    * Reconfigured to use typescript
    * Added basic code for parsing TRX

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,70 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import * as core from '@actions/core';
+import fs from 'fs';
+import path from 'path';
+import xml2js from 'xml2js';
 
-const run = () => {
+const getAbsolutePaths = (fileNames: string[], directoryName: string): string[] => {
+  const absolutePaths: string[] = [];
+
+  for (const file of fileNames) {
+    const absolutePath = path.join(directoryName, file);
+    absolutePaths.push(absolutePath);
+  }
+
+  return absolutePaths;
+};
+
+const getFiles = (trxPath: string): string[] => {
+  console.log(`TRX Path: ${trxPath}`);
+  if (!fs.existsSync(trxPath)) {
+    return [];
+  }
+
+  const fileNames = fs.readdirSync(trxPath);
+  console.log(`Files count: ${fileNames.length}`);
+  const trxFiles = fileNames.filter(f => f.endsWith('.trx'));
+  console.log(`TRX Files count: ${trxFiles.length}`);
+  const filesWithAbsolutePaths = getAbsolutePaths(trxFiles, trxPath);
+  filesWithAbsolutePaths.forEach(f => console.log(`File: ${f}`));
+  return filesWithAbsolutePaths;
+};
+
+const readNodeData = (node: any): any => {
+  return node['$'];
+};
+
+const getElapsedTime = (trx: any): number => {
+  const times = trx.TestRun?.Times;
+
+  if (times && times.length) {
+    const data = readNodeData(times[0]);
+    const start = new Date(data.start);
+    console.log(start);
+    const finish = new Date(data.finish);
+    console.log(finish);
+    var milisconds = finish.getTime() - start.getTime();
+    console.log(milisconds);
+    return milisconds;
+  }
+
+  return 0;
+};
+
+async function run(): Promise<void> {
   try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = new Date().toTimeString();
-    core.setOutput('time', time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
+    const trxPath = core.getInput('test-results');
+    const filePaths = getFiles(trxPath);
+
+    for (const path of filePaths) {
+      const parser = new xml2js.Parser();
+      const file = fs.readFileSync(path);
+      const result = await parser.parseStringPromise(file);
+      getElapsedTime(result);
+    }
   } catch (error: any) {
+    console.log(error);
     core.setFailed(error.message);
   }
-};
+}
 
 run();
