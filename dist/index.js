3bedfe8edb54a945f58ea4d7843a603c80dced6e
bibipkins <bibipkins@gmail.com>
2022-12-08 00:22:04 +0200

    Parsing results and definitions

--- dist/index.js
+++ dist/index.js
@@ -200,7 +200,14 @@ class TrxParser {
             }
             const { start, finish } = this.parseElapsedTime(file);
             const { outcome, total, passed, failed, executed } = this.parseSummary(file);
-            this.parseResults(file);
+            const results = this.parseResults(file);
+            const definitions = this.parseDefinitions(file);
+            results.forEach(element => {
+                console.log(element);
+            });
+            definitions.forEach(element => {
+                console.log(element);
+            });
             const elapsed = finish.getTime() - start.getTime();
             const skipped = total - executed;
             const success = failed === 0 && outcome === 'Completed';
@@ -215,19 +222,45 @@ class TrxParser {
     }
     parseSummary(file) {
         const summary = file.TestRun.ResultSummary[0];
-        const data = summary['$'];
+        const outcome = summary['$'].outcome;
         const counters = summary.Counters[0]['$'];
         const total = Number(counters.total);
         const passed = Number(counters.passed);
         const failed = Number(counters.failed);
         const executed = Number(counters.executed);
-        return { outcome: data.outcome, total, passed, failed, executed };
+        return { outcome, total, passed, failed, executed };
     }
     parseResults(file) {
         const results = file.TestRun.Results[0].UnitTestResult;
-        for (const result of results) {
-            console.log(result['$']);
-        }
+        return results.map(result => ({
+            executionId: result['$'].executionId,
+            testId: result['$'].testId,
+            testName: result['$'].testName,
+            testType: result['$'].testType,
+            testListId: result['$'].testListId,
+            computerName: result['$'].computerName,
+            duration: result['$'].duration,
+            startTime: result['$'].startTime,
+            endTime: result['$'].endTime,
+            outcome: result['$'].outcome,
+            relativeResultsDirectory: result['$'].relativeResultsDirectory
+        }));
+    }
+    parseDefinitions(file) {
+        const definitions = file.TestRun.TestDefinitions[0].UnitTest;
+        return definitions.map(definition => ({
+            id: definition['$'].id,
+            name: definition['$'].name,
+            storage: definition['$'].storage,
+            description: definition.Description[0],
+            executionId: definition.Execution[0]['$'].id,
+            testMethod: {
+                codeBase: definition.TestMethod[0]['$'].codeBase,
+                adapterTypeName: definition.TestMethod[0]['$'].adapterTypeName,
+                className: definition.TestMethod[0]['$'].className,
+                name: definition.TestMethod[0]['$'].name
+            }
+        }));
     }
 }
 exports["default"] = TrxParser;
--- src/parsers/TrxParser.ts
+++ src/parsers/TrxParser.ts
@@ -11,7 +11,15 @@ export default class TrxParser implements IResultParser {
 
     const { start, finish } = this.parseElapsedTime(file);
     const { outcome, total, passed, failed, executed } = this.parseSummary(file);
-    this.parseResults(file);
+    const results = this.parseResults(file);
+    const definitions = this.parseDefinitions(file);
+
+    results.forEach(element => {
+      console.log(element);
+    });
+    definitions.forEach(element => {
+      console.log(element);
+    });
 
     const elapsed = finish.getTime() - start.getTime();
     const skipped = total - executed;
@@ -31,7 +39,7 @@ export default class TrxParser implements IResultParser {
 
   private parseSummary(file: any) {
     const summary = file.TestRun.ResultSummary[0];
-    const data = summary['$'];
+    const outcome = summary['$'].outcome;
     const counters = summary.Counters[0]['$'];
 
     const total = Number(counters.total);
@@ -39,14 +47,42 @@ export default class TrxParser implements IResultParser {
     const failed = Number(counters.failed);
     const executed = Number(counters.executed);
 
-    return { outcome: data.outcome, total, passed, failed, executed };
+    return { outcome, total, passed, failed, executed };
   }
 
   private parseResults(file: any) {
     const results = file.TestRun.Results[0].UnitTestResult;
 
-    for (const result of results) {
-      console.log(result['$']);
-    }
+    return results.map(result => ({
+      executionId: result['$'].executionId,
+      testId: result['$'].testId,
+      testName: result['$'].testName,
+      testType: result['$'].testType,
+      testListId: result['$'].testListId,
+      computerName: result['$'].computerName,
+      duration: result['$'].duration,
+      startTime: result['$'].startTime,
+      endTime: result['$'].endTime,
+      outcome: result['$'].outcome,
+      relativeResultsDirectory: result['$'].relativeResultsDirectory
+    }));
+  }
+
+  private parseDefinitions(file: any) {
+    const definitions = file.TestRun.TestDefinitions[0].UnitTest;
+
+    return definitions.map(definition => ({
+      id: definition['$'].id,
+      name: definition['$'].name,
+      storage: definition['$'].storage,
+      description: definition.Description[0],
+      executionId: definition.Execution[0]['$'].id,
+      testMethod: {
+        codeBase: definition.TestMethod[0]['$'].codeBase,
+        adapterTypeName: definition.TestMethod[0]['$'].adapterTypeName,
+        className: definition.TestMethod[0]['$'].className,
+        name: definition.TestMethod[0]['$'].name
+      }
+    }));
   }
 }
